fb8979ac5deadd7a177ce114bf8c0148e703caae
Ruan Pearce-Authers <ruanpa@outlook.com>
2015-11-07 22:00:21 +0000

    Whitespace

--- test/auth.js
+++ test/auth.js
@@ -11,7 +11,7 @@ describe('auth', function()
 	it('should allow a user to authenticate and then use an authenticated endpoint', function(cb)
 	{
 		helpers.sequence([
-			{ path: '/auth', params: { userID: 'ruan' }, test: res =>  assert.equal(res.data.ok, true) },
+			{ path: '/auth', params: { userID: 'ruan' }, test: res => assert.equal(res.data.ok, true) },
 			{ path: '/system/time', test: res => assert(res.data.time) },
 		], cb)
 	})
